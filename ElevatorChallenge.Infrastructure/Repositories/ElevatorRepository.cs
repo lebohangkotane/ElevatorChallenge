0c7491590d6256f860fb73b4ca8875970ede6d7d
Lebohangkotane <lebohangkotane@gmail.com>
2025-01-22 00:02:31 +0200

    wip: Infrastructure layer

---
+++ ElevatorChallenge.Infrastructure/Repositories/ElevatorRepository.cs
@@ -0,0 +1,46 @@
+﻿using ElevatorChallenge.Domain.Entities;
+using ElevatorChallenge.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorChallenge.Infrastructure.Repositories
+{
+    public class ElevatorRepository : IElevatorRepository
+    {
+        private List<Elevator> _elevators;
+
+        public ElevatorRepository(List<Elevator> elevators)
+        {
+            _elevators = elevators;
+            // Initialize elevators
+        }
+
+        public Elevator GetElevator(int id)
+        {
+            return _elevators.FirstOrDefault(e => e.Id == id);
+        }
+
+        public void AddPassengers(int passengers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanAddPassengers(int passengers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveToFloor(int floor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateElevator(Elevator elevator)
+        {
+            // Update elevator status
+        }
+    }
+}
---
+++ ElevatorChallenge.Infrastructure/Repositories/InMemoryElevatorRepository.cs
@@ -0,0 +1,36 @@
+﻿using ElevatorChallenge.Domain.Entities;
+using ElevatorChallenge.Domain.Enums;
+using ElevatorChallenge.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorChallenge.Infrastructure.Repositories
+{
+    public class InMemoryElevatorRepository : IElevatorRepository
+    {
+        private readonly Dictionary<int, ElevatorBase> _elevators = new();
+        private readonly IElevatorFactory _elevatorFactory;
+
+        public InMemoryElevatorRepository(IElevatorFactory elevatorFactory)
+        {
+            _elevatorFactory = elevatorFactory;
+            InitializeElevators();
+        }
+
+        private void InitializeElevators()
+        {
+            _elevators.Add(1, _elevatorFactory.CreateElevator(ElevatorType.Passenger, 1));
+            _elevators.Add(2, _elevatorFactory.CreateElevator(ElevatorType.Passenger, 2));
+            _elevators.Add(3, _elevatorFactory.CreateElevator(ElevatorType.HighSpeed, 3));
+            _elevators.Add(4, _elevatorFactory.CreateElevator(ElevatorType.Freight, 4));
+            _elevators.Add(5, _elevatorFactory.CreateElevator(ElevatorType.Glass, 5));
+        }
+
+        public IEnumerable<ElevatorBase> GetAll() => _elevators.Values;
+        public ElevatorBase GetById(int id) => _elevators[id];
+        public void Update(ElevatorBase elevator) => _elevators[elevator.Id] = elevator;
+    }
+}
