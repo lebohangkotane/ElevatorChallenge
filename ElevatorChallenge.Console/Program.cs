6ebaee2cf30ec6a33ab406770df77c3cc3481104
Lebohangkotane <lebohangkotane@gmail.com>
2025-01-24 09:22:28 +0200

    feat: Console

--- ElevatorChallenge.Console/Program.cs
+++ ElevatorChallenge.Console/Program.cs
@@ -1,34 +1,48 @@
-﻿using ElevatorChallenge.Application.Services;
-using ElevatorChallenge.Console;
+﻿using ElevatorChallenge.Application.Interfaces;
+using ElevatorChallenge.Application.Services;
 using ElevatorChallenge.Domain.Interfaces;
+using ElevatorChallenge.Infrastructure.Factories;
 using ElevatorChallenge.Infrastructure.Repositories;
+using ElevatorChallenge.Presentation;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 class Program
 {
-    private static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .CreateLogger();
+
+        try
         {
             var host = Host.CreateDefaultBuilder(args)
+                .UseSerilog() // Use Serilog
                 .ConfigureServices((context, services) =>
                 {
-                    services.AddSingleton<IElevatorRepository, InMemoryElevatorRepository>();
+                    services.AddSingleton<ElevatorChallenge.Application.Interfaces.IElevatorRepository, InMemoryElevatorRepository>();
                     services.AddSingleton<IElevatorDispatcher, ElevatorDispatchService>();
+                    services.AddSingleton<IElevatorFactory, ElevatorFactory>(); 
                     services.AddSingleton<ConsoleUI>();
-
-                    services.AddLogging(builder =>
-                    {
-                        builder.AddConsole();
-                        builder.AddDebug();
-                    });
+                    services.AddSingleton(Log.Logger);
+                    services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
                 })
                 .Build();
 
             var ui = host.Services.GetRequiredService<ConsoleUI>();
             await ui.Run();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex.Message);
+            throw;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
         }
     }
 }
